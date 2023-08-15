c733d123ba437eac0ce949ae316fa7358aec54c8
Thiago Henrique De Souza Marasco <101678383+ThiagoMarasco@users.noreply.github.com>
2023-08-15 18:55:47 -0300

    Add files via upload

---
+++ inicial.js
@@ -0,0 +1,45 @@
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled'); // Adiciona uma classe quando a página é rolada
+    } else {
+        nav.classList.remove('scrolled'); // Remove a classe quando a página volta ao topo
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const botaoMostrar = document.getElementById("mostrar-conteudo");
+    const conteudo = document.getElementById("conteudo");
+
+    botaoMostrar.addEventListener("click", function() {
+        if (conteudo.style.display === "none") {
+            conteudo.style.display = "flex"; // Muda para block para exibir
+        } else {
+            conteudo.style.display = "none"; // Muda para none para esconder
+        }
+    });
+
+    window.addEventListener("scroll", function() {
+        conteudo.style.display = "none"; // Esconde o conteúdo ao redimensionar a tela
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const botao = document.getElementById("infos");
+    const conteudo = document.getElementById("fase-1");
+    const conteudo1 = document.getElementById("fase-2");
+
+    conteudo.addEventListener("click", function() {
+        conteudo.classList.toggle('hidden');
+        conteudo1.classList.toggle("hidden");
+    });
+    conteudo1.addEventListener("click", function() {
+        conteudo.classList.toggle('hidden');
+        conteudo1.classList.toggle("hidden");
+    });
+
+
+
+});
+
